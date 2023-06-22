d9f46f665b36ae9219428a3bc4b1455d6aad12c1
jola-blvhk <awujoolabello@gmail.com>
2023-06-22 11:16:09 +0100

    audiophile

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -44,7 +44,7 @@ export const Footer = () => {
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
-            our demo facility - we’re open 7 days a week.
+            our demo facility - we&apos;re open 7 days a week.
           </div>
           <div className="hidden lg:flex justify-between w-32 lg:items-end">
             <div>
