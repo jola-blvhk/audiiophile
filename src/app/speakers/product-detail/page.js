bcb397a0d65b6bf0882dbbcd0bec455ffe949d4f
jola-blvhk <awujoolabello@gmail.com>
2023-06-26 17:26:56 +0100

    imported use state

--- src/app/speakers/product-detail/page.js
+++ src/app/speakers/product-detail/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import Data from "../../../data.json";
 import {
