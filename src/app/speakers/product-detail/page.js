2bbafb3e6e1d1a5c4d34a2e7bb4564a115f00cd5
jola-blvhk <awujoolabello@gmail.com>
2023-07-29 23:27:32 +0100

    add to cart functionality

--- src/app/speakers/product-detail/page.js
+++ src/app/speakers/product-detail/page.js
@@ -3,7 +3,7 @@
 import React, { useContext, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import Data from "../../../data.json";
-import { CartContext } from "@/contexts/CartContext";
+
 import {
   BeforeFooter,
   Button,
